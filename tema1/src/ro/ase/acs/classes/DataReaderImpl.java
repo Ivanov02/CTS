e740c4d065fc3c4ed78175041b41081530e6ab73
IvanovF <ivanovflorin2@yahoo.com>
2023-03-07 23:47:55 +0200

    adaugare public static final Strings

--- tema1/src/ro/ase/acs/classes/DataReaderImpl.java
+++ tema1/src/ro/ase/acs/classes/DataReaderImpl.java
@@ -8,19 +8,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataReaderImpl implements DataReader {
+    private static final String ID = "id: ";
+    private static final String ADDRESS = "address: ";
+    private static final String SALARY = "id: ";
+
+
     @Override
     public void readData(Connection connection) throws SQLException {
         String sqlSelect = "SELECT * FROM employees";
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(sqlSelect);
         while(rs.next()) {
-            int id = rs.getInt("id");
+            int id = rs.getInt(ID);
             System.out.println("id: " + id);
             String name = rs.getString(2);
             System.out.println("name: " + name);
-            String address = rs.getString("address");
+            String address = rs.getString(ADDRESS);
             System.out.println("address: " + address);
-            double salary = rs.getDouble("salary");
+            double salary = rs.getDouble(SALARY);
             System.out.println("salary: " + salary);
         }
         rs.close();
